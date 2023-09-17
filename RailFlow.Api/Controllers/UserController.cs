137aeae2bafa585ccd678afc62c260094da0d2e5
Michallenort <michal.lenort@gmail.com>
2023-09-17 22:48:16 +0200

    Add Sign In option.

--- RailFlow.Api/Controllers/UserController.cs
+++ RailFlow.Api/Controllers/UserController.cs
@@ -1,7 +1,9 @@
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RailFlow.Application.Security;
 using RailFlow.Application.Users.Commands;
+using RailFlow.Application.Users.DTO;
 
 namespace RailFlow.API.Controllers;
 
@@ -10,10 +12,12 @@ namespace RailFlow.API.Controllers;
 public class UserController : ControllerBase
 {
     private readonly IMediator _mediator;
-    
-    public UserController(IMediator mediator)
+    private readonly ITokenStorage _tokenStorage;
+
+    public UserController(IMediator mediator, ITokenStorage tokenStorage)
     {
         _mediator = mediator;
+        _tokenStorage = tokenStorage;
     }
 
     [AllowAnonymous]
@@ -22,5 +26,14 @@ public class UserController : ControllerBase
     {
         await _mediator.Send(command);
         return NoContent();        
+    }
+    
+    [AllowAnonymous]
+    [HttpPost("sign-in")]
+    public async Task<ActionResult<JwtDto>> SignIn(SignIn command)
+    {
+        await _mediator.Send(command);
+        var jwt = _tokenStorage.Get();
+        return Ok(jwt);
     }
 }
